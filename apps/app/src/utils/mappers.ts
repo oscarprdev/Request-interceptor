03a02657aba0d33446ec7cdedd649d2cddeea94f
oscarprdev <vlcoscar8@gmail.com>
2025-03-11 21:04:19 +0100

    Add mapper function to convert rule application to server format

--- apps/app/src/utils/mappers.ts
+++ apps/app/src/utils/mappers.ts
@@ -1,5 +1,6 @@
 import type { Rule, RuleApplication } from '@/models/Rule';
 import { formatPgDate } from './dates';
+import type { RuleToServer } from '@/services/mutations/rules-mutations';
 
 export const mapRuleToApplication = (rule: Rule): RuleApplication => ({
   id: rule.id,
@@ -12,4 +13,14 @@ export const mapRuleToApplication = (rule: Rule): RuleApplication => ({
   response: JSON.parse(atob(rule.redirectUrl?.split(',')[1] || '{}')),
   createdAt: formatPgDate(rule.createdAt),
   updatedAt: formatPgDate(rule.updatedAt),
+});
+
+export const mapRuleToServer = (rule: RuleApplication): RuleToServer => ({
+  redirectUrl: 'data:application/json;base64,' + btoa(JSON.stringify(rule.response)),
+  requestMethods: rule.requestMethods,
+  urlFilter: rule.urlFilter,
+  resourceTypes: rule.resourceTypes,
+  actionType: rule.actionType,
+  priority: rule.priority ?? 3,
+  isEnabled: rule.isEnabled ?? false,
 });
