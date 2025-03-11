b843c2c61abe421a5655a229ebebe473e90d257f
oscarprdev <vlcoscar8@gmail.com>
2025-03-11 21:03:55 +0100

    Refactor rule creation mutation with improved API endpoint and type safety

--- apps/app/src/services/mutations/rules-mutations.ts
+++ apps/app/src/services/mutations/rules-mutations.ts
@@ -1,14 +1,14 @@
 import type { Rule } from '@/models/Rule';
 import { API_URL } from '../common';
 
+export type RuleToServer = Omit<Rule, 'id' | 'createdAt' | 'updatedAt'>;
+
 export const rulesMutations = {
-  createRule: async ({ collectionId, rule }: { collectionId: string; rule: Rule }) => {
-    const response = await fetch(`${API_URL}/collections/${collectionId}/rules`, {
+  createRule: async ({ rule, collectionId }: { collectionId: string; rule: RuleToServer }) => {
+    const response = await fetch(`${API_URL}/rules/${collectionId}`, {
       method: 'POST',
       body: JSON.stringify(rule),
     });
-    const data = await response.json();
-    console.log(data);
-    return data;
+    return await response.json();
   },
 };
