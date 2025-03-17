d21bfd473b654bc73628e3b4d84e222df3eef188
oscarprdev <vlcoscar8@gmail.com>
2025-03-17 18:10:39 +0100

    Remove compiled output files from the server's dist directory and update .gitignore to include the dist folder for better build management.

--- apps/server/dist/routes/ruleCollectionsRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const controllers_1 = require("@/controllers");
-const router = (0, express_1.Router)();
-router.get('/count/:collectionId', controllers_1.ruleCollectionsController.countRulesByCollection.bind(controllers_1.ruleCollectionsController));
-exports.default = router;
-//# sourceMappingURL=ruleCollectionsRoutes.js.map
