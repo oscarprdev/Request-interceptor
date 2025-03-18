929509dd51606fb0e6651bcde5799f24204b5a35
oscarprdev <vlcoscar8@gmail.com>
2025-03-18 07:36:44 +0100

    Update API URL in common service to point to Railway deployment

--- apps/app/src/services/common.ts
+++ apps/app/src/services/common.ts
@@ -1,2 +1,2 @@
 // export const API_URL = 'http://localhost:8080/api/v1';
-export const API_URL = 'https://requestick-backend.vercel.app/api/v1';
+export const API_URL = 'https://requestick.up.railway.app/api/v1';
