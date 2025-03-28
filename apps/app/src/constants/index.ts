061e1988ac49d402505039022863019b3f7ad380
oscarprdev <vlcoscar8@gmail.com>
2025-03-28 06:38:45 +0100

    Update API URL to include versioning for improved endpoint clarity

--- apps/app/src/constants/index.ts
+++ apps/app/src/constants/index.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'https://requestick.up.railway.app';
+export const API_URL = 'https://requestick.up.railway.app/api/v1';
 export const EXTENSION_ID = 'hjnfioocbpcjcmlhegpdbkpbaoifbime';
