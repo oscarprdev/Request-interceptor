641f7c79fd783824bb0b8c7516c6d4ba4c9832ab
oscarprdev <vlcoscar8@gmail.com>
2025-04-08 18:55:35 +0200

    Update allowedOrigins in environment configuration to support multiple default origins for improved flexibility

--- apps/server/src/config/environment.ts
+++ apps/server/src/config/environment.ts
@@ -8,5 +8,9 @@ export const config = {
   port: parseInt(process.env.PORT || '3001', 10),
   databaseUrl:
     process.env.DATABASE_URL || 'postgresql://postgres:password@localhost:5432/request_interceptor',
-  allowedOrigins: (process.env.ALLOWED_ORIGINS || 'http://localhost:3000').split(','),
+  allowedOrigins: (
+    process.env.ALLOWED_ORIGINS ||
+    'http://localhost:3000' ||
+    'http://localhost:5173'
+  ).split(','),
 };
